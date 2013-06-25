848019a05408f0065705258d073e05c3c15b953a
bcrowe <bryan@zapsolutions.com>
2013-06-24 21:23:32 -0400

    move controller private functions into model methods

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -30,9 +30,9 @@ class GamesController extends AppController
 			$data['Player'][$data['Action']['key']]['action'] = $data['Action']['type'];
 			$this->Game->create();
 			if ($this->Game->saveAll($data)) {
-				$this->__updateStats($data['Player'], $data['Action']['type']);
-				$this->__updateRatings($data['Player']);
-				$this->__updateStreaks($data['Player']);
+				$this->User->updateStats($data['Player'], $data['Action']['type']);
+				$this->User->updateRatings($data['Player']);
+				$this->User->updateStreaks($data['Player']);
 				$this->Session->setFlash(
 					__('The %s has been saved!', __('game')),
 					'alert',
@@ -58,75 +58,6 @@ class GamesController extends AppController
 				'order'  => array('User.name ASC')
 			));
 			$this->set(compact('users'));
-		}
-	}
-
-	private function __updateStats($players = array(), $action = null)
-	{
-		foreach ($players as $player) {
-			if ($player['result'] === '0') {
-				$this->User->id = $player['user_id'];
-				$losses = $this->User->field('losses') + 1;
-				$this->User->saveField('losses', $losses);
-			}
-			if ($player['result'] === '1') {
-				$this->User->id = $player['user_id'];
-				$wins = $this->User->field('wins') + 1;
-				$this->User->saveField('wins', $wins);
-			}
-			if (isset($player['actor']) && $player['actor'] === true) {
-				$this->User->id = $player['user_id'];
-				$actions = $this->User->field($action . 's') + 1;
-				$this->User->saveField($action . 's', $actions);
-			}
-		}
-	}
-	private function __updateRatings($players = array())
-	{
-		foreach ($players as $player) {
-			$user = $this->User->find('first', array(
-				'conditions' => array(
-					'User.id' => $player['user_id']
-				)
-			));
-			$augWins = $user['User']['wins'] + ($user['User']['sinks'] * 0.50) + ($user['User']['hits'] * 0.25);
-			$augLosses = $user['User']['losses'] + ($user['User']['tks'] * 0.50) + $user['User']['decay'];
-			$augSpread = $augWins - $augLosses;
-			if ($augWins - $augLosses > 0 || $augWins - $augLosses === 0) {
-				$percentage = $user['User']['wins'] / $user['User']['total_games'];
-			} else {
-				$percentage = $user['User']['losses'] / $user['User']['total_games'];
-			}
-			$rating = $percentage * $augSpread;
-			$this->User->id = $player['user_id'];
-			$this->User->saveField('rating', $rating);
-		}
-	}
-
-	private function __updateStreaks($players = array())
-	{
-		foreach ($players as $player) {
-			$user = $this->User->find('first', array(
-				'conditions' => array(
-					'User.id' => $player['user_id']
-				)
-			));
-			if ($player['result'] === '1') {
-				if ($user['User']['streak'] > 0 || $user['User']['streak'] == 0) {
-					$streak = $user['User']['streak'] + 1;
-				} else {
-					$streak = 1;
-				}
-			}
-			if ($player['result'] === '0') {
-				if ($user['User']['streak'] < 0 || $user['User']['streak'] == 0) {
-					$streak = $user['User']['streak'] - 1;
-				} else {
-					$streak = -1;
-				}
-			}
-			$this->User->id = $player['user_id'];
-			$this->User->saveField('streak', $streak);
 		}
 	}
 
--- app/Model/Game.php
+++ app/Model/Game.php
@@ -28,4 +28,74 @@ class Game extends AppModel {
 		)
 	);
 
+	public function updateRatings($players = array())
+	{
+		foreach ($players as $player) {
+			$user = $this->find('first', array(
+				'conditions' => array(
+					'User.id' => $player['user_id']
+				)
+			));
+			$augWins = $user['User']['wins'] + ($user['User']['sinks'] * 0.50) + ($user['User']['hits'] * 0.25);
+			$augLosses = $user['User']['losses'] + ($user['User']['tks'] * 0.50) + $user['User']['decay'];
+			$augSpread = $augWins - $augLosses;
+			if ($augWins - $augLosses > 0 || $augWins - $augLosses === 0) {
+				$percentage = $user['User']['wins'] / $user['User']['total_games'];
+			} else {
+				$percentage = $user['User']['losses'] / $user['User']['total_games'];
+			}
+			$rating = $percentage * $augSpread;
+			$this->id = $player['user_id'];
+			$this->saveField('rating', $rating);
+		}
+	}
+
+	public function updateStats($players = array(), $action = null)
+	{
+		foreach ($players as $player) {
+			if ($player['result'] === '0') {
+				$this->id = $player['user_id'];
+				$losses = $this->field('losses') + 1;
+				$this->saveField('losses', $losses);
+			}
+			if ($player['result'] === '1') {
+				$this->id = $player['user_id'];
+				$wins = $this->field('wins') + 1;
+				$this->saveField('wins', $wins);
+			}
+			if (isset($player['actor']) && $player['actor'] === true) {
+				$this->id = $player['user_id'];
+				$actions = $this->field($action . 's') + 1;
+				$this->saveField($action . 's', $actions);
+			}
+		}
+	}
+
+	public function updateStreaks($players = array())
+	{
+		foreach ($players as $player) {
+			$user = $this->find('first', array(
+				'conditions' => array(
+					'User.id' => $player['user_id']
+				)
+			));
+			if ($player['result'] === '1') {
+				if ($user['User']['streak'] > 0 || $user['User']['streak'] == 0) {
+					$streak = $user['User']['streak'] + 1;
+				} else {
+					$streak = 1;
+				}
+			}
+			if ($player['result'] === '0') {
+				if ($user['User']['streak'] < 0 || $user['User']['streak'] == 0) {
+					$streak = $user['User']['streak'] - 1;
+				} else {
+					$streak = -1;
+				}
+			}
+			$this->id = $player['user_id'];
+			$this->saveField('streak', $streak);
+		}
+	}
+
 }
