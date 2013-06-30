8f5ea676b03e7468e3fae5d18c553461bf400044
bcrowe <bryan@zapsolutions.com>
2013-06-29 23:02:33 -0400

    additional work on decay system

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -47,16 +47,22 @@ class ApiController extends AppController
 			throw new MethodNotAllowedException('Derp.');
 		}
 
+		$oneWeekAgo = date ('Y-m-d H:i:s', strtotime ('-1 week', time())); 
+
 		$users = $this->User->find('all', array(
 			'conditions' => array(
-				'User.modified >=' => //$now + 7 days;
+				'User.modified <=' => $oneWeekAgo
 			)
 		));
 		
 		foreach ($users as $user) {
-			// add decay
-			// update rating
+			$this->User->id = $user['User']['id'];
+			$currentDecay = $this->User->field('decay');
+			$this->User->saveField('decay', $currentDecay + 1);
 		}
+	
+		$this->loadModel('Game');
+		//$this->Game->updateRatings();
 	}
 
 }
