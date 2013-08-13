e68783745553c3f151d07156e58d3f3ec17ede6b
Bryan Crowe <bryan@zapsolutions.com>
2013-08-12 22:00:08 -0400

    Updated some white spacing

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -20,16 +20,16 @@ class ApiController extends AppController
 
 		$champion = $this->User->find('all', array(
 			'conditions' => array('User.active' => 1),
-			'fields' => array('name', 'wins', 'losses'),
-			'order' => array('User.rating DESC'),
-			'limit' => 1
+			'fields'     => array('name', 'wins', 'losses'),
+			'order'      => array('User.rating DESC'),
+			'limit'      => 1
 		));
 
 		$unchampion = $this->User->find('all', array(
 			'conditions' => array('User.active' => 1),
-			'fields' => array('name', 'wins', 'losses'),
-			'order' => array('User.rating ASC'),
-			'limit' => 1
+			'fields'     => array('name', 'wins', 'losses'),
+			'order'      => array('User.rating ASC'),
+			'limit'      => 1
 		));
 
 		$data['Champion'] = $champion;
