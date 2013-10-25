c98c00396edc9428934306bb337d153d0011d877
Bryan Crowe <bryan@zapsolutions.com>
2013-10-25 18:19:37 -0400

    Fix API response for website

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -31,7 +31,7 @@ class ApiController extends AppController
 		));
 
 		$unchampion = $this->User->find('all', array(
-			'conditions' => array('User.active' => 1),
+			'conditions' => array('User.active' => 1, 'User.losses >' => 1),
 			'fields'     => array('name', 'wins', 'losses'),
 			'order'      => array('User.rating ASC'),
 			'limit'      => 1
